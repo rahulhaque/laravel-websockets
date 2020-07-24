54bfe7d8273f1b0f5b83ed2670fcbc78b315537b
Rahul Haque <rahulhaque07@gmail.com>
2020-07-24 23:33:19 +0600

    config/websockets.php typo fix

--- config/websockets.php
+++ config/websockets.php
@@ -29,7 +29,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
-            'origin' => [],
+            'origins' => [],
             'enable_client_messages' => false,
             'enable_statistics' => true,
         ],
